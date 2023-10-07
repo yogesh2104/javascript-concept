a78ea8a1259ee40c5b040162c57977d5a71a7047
Yogesh Singh <singhyogesh2104@gmail.com>
2023-10-07 22:50:34 +0530

    [learn] JS truthy value

---
+++ main.js
@@ -0,0 +1,16 @@
+// [] is ![]
+// console.log('Array is equal not array',[]==![]) 
+// equality operater convert both side into number then compare both side and both side is 0 
+// for different reason Array Truthy, on the right true convert into false which is 0 and empty array is concerd to 0 then its becomes true
+// ! [] is truthy not true
+
+// console.log("+[]== +![]",+[]== +![]) // true
+// console.log("0== +false",0 == +false) +sign try to convert into number
+
+// console.log("true == ![]",true ==![]) //false
+
+// console.log(true == ![]) //false
+
+// console.log("!!'false'== !!'true'",!!'false'== !!'true') //true
+
+// console.log("baNaNa",'b' + 'a' + +'a' + 'a') // baNana
