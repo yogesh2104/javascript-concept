8b4e53c85a16f5719e6b82908dec3fbd463cf94c
yogesh2104 <singhyogesh2104@gmail.com>
2024-01-03 23:50:15 +0530

    learn about array and date&time

---
+++ array.js
@@ -0,0 +1,40 @@
+//array 
+
+// let myArr= [1,2,3,4,5,true,null]
+
+//in javascript array are resizable and mixed type 
+// javascript array copy operation are shallow copy not deep copy
+// shallow copy is share same refrence point as those of the source object form which copy was made
+// means when i have change copy array its change original aarray value
+// ohter hand deep copy not share same refrence point ant not change original array
+
+
+// array method 
+// push value in array 
+const myarray= [1,2,3,4,5]
+// myarray.push(7)
+// console.log("array", myarray)
+
+const myarr=new Array(1,2,3,4,5)
+// console.log("myarr", myarr)
+
+// console.log("POP arr",myarr.pop()) // remove last element
+// unshift and shift
+// includes ---> this return true are false 
+// join() ---> this join two array and return stings type
+
+// const newArr=[1,2,3,4,5]
+// const arr2 = [5,6,7,9]
+// const joinArr=newArr.join()
+
+// console.log(joinArr)
+
+//slice,splice
+
+const myArr1 = [2,3,4,5,6,7,8]
+// console.log(myArr1.slice()) // return section of array two param need start and end and most important slice not modify original array
+// otherhand splice change original array 
+
+
+
+
---
+++ date&time.js
@@ -0,0 +1,7 @@
+// date 
+// javascript date object representing a single moment of time in a platform independent formate. consider date form 1 jan 1970 UTC 
+
+const moment = new Date()
+// console.log(moment) //2024-01-02T18:48:43.581Z
+
+console.log(moment.toString())
