42cadcb00965148b06ea54f070f3270611883ea5
yogesh2104 <singhyogesh2104@gmail.com>
2024-05-16 07:44:04 +0530

    learn about  object

--- object.js
+++ object.js
@@ -33,7 +33,7 @@ const obj1={a:"a",b:"b",c:"c"}
 const obj2={d:"a",e:"b",f:"c"}
 
 const obj3=Object.assign({},obj1,obj2)
-console.log(obj3)
+// console.log(obj3)
 
-console.log(singletonObject)
+// console.log(singletonObject)
 
