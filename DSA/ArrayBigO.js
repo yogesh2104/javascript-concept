839e69e5dc6c82e16d2025f7f5e745851968899d
yogesh2104 <singhyogesh2104@gmail.com>
2024-06-02 22:32:51 +0530

    learn about object and array time complexity and there method

---
+++ DSA/ArrayBigO.js
@@ -0,0 +1,13 @@
+//! An array is an ordered collections of values 
+
+
+const odd=[1,3,4,5,6,71]
+
+// insert/remove at end is O(1) -- constant
+// insert/remove at beginning is O(n) -- linear
+// Access O(1)
+// Search O(n)
+// Push/Pop is O(1)
+// shift/unshift/conact/slice/splice is O(n)
+// forEach/map/filter/reduce is O(n)
+
---
+++ DSA/ObjectBigO.js
@@ -0,0 +1,14 @@
+// ! An Object is collection of key and values pairs.
+
+/*  const Person={
+    'name': 'John',
+    'age':23
+} */
+//  Insert inot value into Obejct is  always Constant O(1)  
+//  delete value from Obejct is always Constant O(1)  
+//  Access value into Obejct is  always Constant O(1)  
+// Search value into Object is liner O(n)
+
+// Object.keys() --- O(n)
+// Object.values() --- O(n)
+// Object.entries() --- O(n)
